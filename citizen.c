2590a38d941851d44ef5ccb53c191d461ce4b460
georgeaz <azzamgeorge@cmapus.technion.ac.il>
2018-12-03 14:01:38 +0200

    everything were changed 2 citizen is changed CITIZEN changeinfo func should be implemented
    Signed-off-by: georgeaz <azzamgeorge@cmapus.technion.ac.il>

--- citizen.c
+++ citizen.c
@@ -1,160 +1,106 @@
 //
-// Created by User on 28/11/2018.
+// Created by George on 11/29/2018.
 //
-
-
-#include <stdio.h>
-#include <stdbool.h>
-#include <assert.h>
+#include "citizen.h"
+#include "uniqueOrderedList/uniqueOrderedList.h"
 #include <stdlib.h>
-#include "City.h"
-
-struct City_t{
-    Id id;
-    Name name;
-    Set citizens;
-    Set candidates;
+//we should include this in general functions
+struct Citizen_t{
+  Name name;
+  Age age;
+  Id id;
+  EducationYears education_years;
+  UniqueOrderedList citizen_prefrences;
 };
-
-Id CityGetId(City city){
-    return city->id;
-}
-Name CityGetName(City city){
-    return  city->name;
-}
-void CityDestroy(City city){
-    free(city->name);
-    free(city->id);
-    setDestroy(city->citizens);
-    free(city);
-}
-City CityCreate(){
-    //Name was: Name*, Name is char**
-    // if(name==NULL || city==NULL || id==NULL) What happens to the other two arguments?they are not freed
-    //    return NULL;
-    City city=(City)malloc(sizeof(*city));
-    Set citizens=setCreate((copySetElements)CitizenCopy,CitizenDestroy,CitizenCompere);
+Citizen CitizenCreate(){
+    //Name name=(Name)malloc(sizeof(*name));
+    //malloc for name will be added in copy, since we dont know the size of name
+    Citizen citizen=(Citizen)malloc(sizeof(*citizen));
+    Age age=(Age)malloc(sizeof(*age));
     Id id=(Id)malloc(sizeof(*id));
-    if(city==NULL||citizens==NULL||id==NULL){
-        free(city);
-        setDestroy(citizens);
+    EducationYears education_years=(EducationYears)malloc(sizeof(*education_years));
+    UniqueOrderedList citizen_prefrences=uniqueOrderedListCreate(main,main,main,main);
+    if(citizen==NULL||age==NULL||id==NULL||education_years==NULL||citizen_prefrences==NULL){
+        free(citizen);
+        free(age);
         free(id);
+        free(education_years);
+        uniqueOrderedListDestroy(citizen_prefrences);
         return NULL;
     }
-    city->id=id;
-    city->citizens=citizens;
-    return city;
+    citizen->age=age;
+    citizen->id=id;
+    citizen->education_years=education_years;
+    citizen->citizen_prefrences=citizen_prefrences;
+    return citizen;
 }
-City CityCopy(City source_city){
-    //if were added
-    if(source_city==NULL)
+void CitizenDestroy(Citizen citizen)
+{
+    //always check if  object==NULL before accessing struct fields!
+    if(citizen==NULL)return NULL;
+    free(citizen->age);
+    free(citizen->education_years);
+    free(citizen->id);
+    uniqueOrderedListDestroy(citizen->citizen_prefrences);
+    free(citizen);
+
+}
+Citizen CitizenCopy(Citizen source_citizen){
+    if(source_citizen==NULL)return NULL;
+    Citizen new_citizen=CitizenCreate();
+    if(new_citizen==NULL){
+        CitizenDestroy(new_citizen);
         return NULL;
-    City new_city= CityCreate();
-    String name_copy=malloc(sizeof(Name)*Stringlength(*source_city->name));
-    if(new_city==NULL||name_copy==NULL){
-        CityDestroy(new_city);
-        return NULL;
     }
-    new_city->name=StringCopy(*new_city->name);
-    new_city->citizens=source_city->citizens;
-    new_city->id=source_city->id;
-    return new_city;
+    //we should check this!
+    new_citizen->name=StringCopy(*new_citizen->name);
+    new_citizen->id=source_citizen->id;
+    new_citizen->education_years=source_citizen->education_years;
+    new_citizen->age=source_citizen->age;
+    //u should make a unique ordered list that return a copy
+    //new_citizen->citizen_prefrences=source_citizen->citizen_prefrences;
+    return new_citizen;
 }
-CitizenResult CityInsertCitizen(City city, Citizen citizen){
-    //the whole function were replaced
-    SetResult result=setAdd(city->citizens,citizen);
-    switch(result){
-        case SET_NULL_ARGUMENT:
-            return CITIZEN_NULL_ARGUMENT;
-        case SET_OUT_OF_MEMORY:
-            return CITIZEN_MEMORY_ERROR;
-        case SET_ITEM_ALREADY_EXISTS:
-            return CITIZEN_ALREADY_EXISTS;
-        case SET_SUCCESS:
-            return CITIZEN_SUCCESS;
-        default:return CITIZEN_SUCCESS;
-    }
+CitizenResult CitizenInsertPrefrence(City city, Citizen citizen,Id candidate,Priority priorty ) {
+    UniqueOrderedList result=uniqueOrderedListInsert(citizen->citizen_prefrences,);
+    //switch(result
+
 }
-CitizenResult CityRemoveCitizen(City city, Citizen citizen){
-    SetResult remove_result=setRemove(city->citizens,citizen);
-    switch (remove_result){
-        case SET_NULL_ARGUMENT:
-            return CITIZEN_NULL_ARGUMENT;
-        case SET_ITEM_DOES_NOT_EXIST:
-            return CITIZEN_DOES_NOT_EXIST;
-        case SET_SUCCESS:
-            return CITIZEN_SUCCESS;
-        default:return CITIZEN_SUCCESS;
+bool CitizenCompere(Citizen old_citizen,Citizen new_citizen){
+    return old_citizen->id==new_citizen->id;
+}
+
+void CitizenGetInformation(Citizen citizen,Information information,CitizenInformation desirable_information) {
+    //للأمانة انا غير متأكد من الدالّة يا لينا الع!
+    switch (desirable_information) {
+        case CITIZEN_ID:information = citizen->id;
+        case CITIZEN_NAME:information = citizen->name;
+        case CITIZEN_EDUCATION_YEARS:information = citizen->education_years;
+        case CITIZEN_AGE:information = citizen->age;
     }
 }
-bool CityCompare(City old_city,City new_city){
-    return old_city->id==new_city->id;
+void CitizenCandidateToBeRemovePrefrences(Citizen citizen){
+    uniqueOrderedListClear(citizen->citizen_prefrences);
 }
-bool CityIsLegal(City city){
-    return (city->id>=0);
-    // if(city==NULL)
-    //   return CITY_NULL_ARGUMENT; you dont have to return null since u already checked that
+void CitizenRemovePrefrence(Citizen citizen, const Id const candidate_id){
+
+
 }
-//why u write GET when u get a bool?!
-bool CityIsCandidateACitizen(City city,int candidate_to_be_id){
-
-    // if(!(setIsIn(city->candidates,candidate)))
-    //  return false;
-    //if u return false here this means the candidate is not a citizen//so the error well send to the mtmelections well be false :(
-    Citizen citizen=CitizenCreate();
-    citizen=CitizenCopy(setGetFirst(city->citizens));
-    while (citizen)
-    {
-        int citizen_id=CitizenGetid(citizen);
-        if(citizen_id==candidate_to_be_id)return true;
-
-    }
-
-    *citizen_id=CitizenGetid(setGetFirst(city->citizens));
-
-
-
-    //return false were added!
-    return false;
+int CitizenGetid(Citizen citizen){
+    return *(citizen->id);
 }
-/*MtmElectionsResult mtmElectionsAddCandidate(MtmElections mtmElections,
- * int candidateId, int cityId);
- * The above functions takes an int
- * City is above candidate, so it gets the int candidateId first, thus u do
- * all the validation, and just after that u create a Candidate object and insert
- * it.
-*/
+//maybe no:
+Citizen CitizenGetCopy(Citizen citizen){
+    Citizen citizen_copy=CitizenCreate();
+    if(citizen_copy==NULL)return NULL;
+    CitizenCopy(citizen);
+    //  citizen_copy->name=StringCopy(citizenName);
 
-CandidateResult CityInsertCandidate(City city, int candidate_id){
-    if(city==NULL)return CANDIDATE_NULL_ARGUMENT;
-    if(candidate_id<FIRST_LEGAL_ID)//we said that we are not using numbers
-        return CANDIDATE_ILLEGAL_ID;
-    //Citizen candidate_to_be=NULL;
-    //we should have malloced him, u cant return a copy of somthing that u havent
-    //created yet!
+    // *(citizen_copy->id)=citizenId;
+    // *(citizen_copy->education_years)=yearsOfEducation;
+    //   *(citizen_copy->age)=citizenAge;
+    //for now its null. well see if well use it somewhere...
+    //  citizen_copy->citizen_prefrences=NULL;
 
-    if(!CityIsCandidateACitizen(city,candidate,candidate_to_be))
-        return CANDIDATE_CITIZEN_DOSE_NOT_EXIST;
-    Age candidate_age=NULL;
-    CitizenGetInformation(candidate_to_citizen,candidate_age,CITIZEN_AGE);
-    if(*candidate_age < CANDIDATE_MINIMUM_AGE){
-        return CANDIDATE_AGE_NOT_APPROPRIATE;
-    }
-    CitizenCandidateToBeRemovePrefrences(candidate_to_citizen);
-    SetResult add_candidate_result=setAdd(city->candidates,candidate);
-    switch (add_candidate_result){
-        case SET_NULL_ARGUMENT:
-            return CANDIDATE_NULL_ARGUMENT;
-        case SET_OUT_OF_MEMORY:
-            return CANDIDATE_MEMORY_ERROR;
-        case SET_ITEM_ALREADY_EXISTS:
-            return CANDIDATE_ALREADY_EXISTS;
-        default:return CANDIDATE_SUCCESS;
-    }
-}
-CandidateResult CityRemoveCandidate(City city,Candidate candidate){
-    SetResult remove_candidate_result=setRemove(city->candidates,candidate);
-    switch (remove_candidate_result){
 
-    }
 }
